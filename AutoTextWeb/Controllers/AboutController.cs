4af422273c1e358fa6de6c6ac10f805355d66121
Justin Wolbeck <justin.wolbeck@gmail.com>
2018-10-17 22:38:34 -0500

    Initial Checkin (Mostly Stock Web App)

---
+++ AutoTextWeb/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AutoTextWeb.Models;
+
+namespace AutoTextWeb.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult TellMeMore()
+        {
+            return View();
+        }
+    }
+}
